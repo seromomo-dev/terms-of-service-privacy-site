1824dfbee018589be4b61d0faf15a6f9d9efc98f
seromomo-dev <seromomo.dev@gmail.com>
2026-01-01 12:09:06 +0900

    🎨コードフォーマット適用

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,10 +1,11 @@
 import { TestBed } from '@angular/core/testing';
+
 import { App } from './app';
 
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [App],
+      imports: [App]
     }).compileComponents();
   });
 
@@ -18,6 +19,8 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, terms-of-service-privacy-site');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Hello, terms-of-service-privacy-site'
+    );
   });
 });
